975c954213f5708f7bd724bbfcece55b60953249
riskiilyas <riskiilyas03@gmail.com>
2023-04-01 12:08:15 +0700

    doing homepage

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -46,6 +46,32 @@ class SessionController extends Controller
 
     }
 
+    public function change_password(Request $request) {
+        $request->validate([
+//            'token' => 'required',
+//            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET
+            ? redirect()->route('login')->with('status', __($status))
+            : back()->withErrors(['email' => [__($status)]]);
+
+    }
+
     public function store(Request $request) {
         $validatedData = $request->validate([
             'username' => 'required|max:20',
