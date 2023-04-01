8232f64967635dc0c167c470689a1a7b310470fa
Mikhael Aryasatya <rythmikhal.f@gmail.com>
2023-04-01 18:18:31 +0700

    Uuuuhhhhhh, eloquent models????

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,16 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Discussion extends Model
+class Comment extends Model
 {
     use HasFactory;
-
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
-
-    public function comment(): HasMany
-    {
-        return $this . hasMany(Comment::class);
+    public function discussion(){
+        return $this->belongsTo(Discussion::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
     public function discussion():HasMany {
         return $this.hasMany(Discussion::class);
     }
+
+    public function comment(): HasMany
+    {
+        return $this.hasMany(Comment::class);
+    }
 }
