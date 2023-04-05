e861f006b24f275d28e37701fbafcc1c18d26c8f
riskiilyas <riskiilyas03@gmail.com>
2023-04-05 19:00:22 +0700

    Upvote, Downvote, Fav, Follow Done

--- resources/views/change_password.blade.php
+++ resources/views/change_password.blade.php
@@ -39,7 +39,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
-                <img src="/images/icon.jpg" alt="" width="30" height="30" class="d-inline-block align-text-top" style="margin-right: 1rem">
+                <img src="/images/icon.png" alt="" width="30" height="30" class="d-inline-block align-text-top" style="margin-right: 1rem">
                 Dizcuzz
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -71,7 +71,7 @@
                         </ul>
                     </li>
                 </ul>
-                <form class="d-flex">
+                <form class="d-flex" method="get" action="/search/discussion">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-primary" type="submit">Search</button>
                 </form>
