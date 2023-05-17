be7d1775ba82c4fafb7c4f4f33531b9d79409889
Fznaf <89885429+Fznaf@users.noreply.github.com>
2023-05-17 20:55:02 +0700

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -78,7 +78,7 @@
                 </label>
             </div>
 
-            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+            <button class="w-100 btn btn-lg btn-danger" type="submit">Sign in</button>
             <p class="mt-5 mb-3 text-muted">&copy; 2023 Dizcuss</p>
         </form>
     </main>
